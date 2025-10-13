98e401c4c9999dfbd08770c7bb0adfbe9c7302a7
Porter Goldman <porter.goldman@outlook.com>
2025-10-12 23:21:41 -0600

    Update top.v

--- Projects/Lab 7/top.v
+++ Projects/Lab 7/top.v
@@ -1,3 +1,5 @@
+
+
 module top (
     input [9:0]sw,
     output[13:0]led
@@ -11,15 +13,20 @@ half_sub half_sub_inst(
     .Borrow(led[1])
 );
 
+    
 ones_compliment ones(
     .A(sw[5:2]),
     .B(sw[9:6]),
     .Y(led[5:2])
 );
 
+    
 twos_compliment two(
     .A(sw[9:2]),
     .Y(led[13:6])
 );
 
+
 endmodule
+
+
