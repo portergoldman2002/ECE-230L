4947b4348ab7aeb150aadf309b6643d484ea7800
Porter Goldman <porter.goldman@outlook.com>
2025-10-12 23:19:49 -0600

    Update ones_compliment.v

--- Projects/Lab 7/ones_compliment.v
+++ Projects/Lab 7/ones_compliment.v
@@ -1,17 +1,19 @@
 
+
 module ones_compliment(
     input [3:0]A,
     input [3:0]B,
     output [3:0]Y
-
 );
 
+    
 wire connector1;
 wire connector2;
 wire connector3;
 wire around;
 wire [3:0] APlusB;
 
+    
 full_adder first_bit_inst(
     .A(A[0]),
     .B(B[0]),
@@ -20,6 +22,7 @@ full_adder first_bit_inst(
     .Y(APlusB[0])
 );
 
+    
 full_adder second_bit_inst(
     .A(A[1]),
     .B(B[1]),
@@ -28,6 +31,7 @@ full_adder second_bit_inst(
     .Y(APlusB[1])
 );
 
+    
 full_adder third_bit_inst(
     .A(A[2]),
     .B(B[2]),
@@ -36,6 +40,7 @@ full_adder third_bit_inst(
     .Y(APlusB[2])
 );
 
+    
 full_adder final_bit_inst(
     .A(A[3]),
     .B(B[3]),
@@ -47,43 +52,48 @@ full_adder final_bit_inst(
 //second stage
 wire[3:0]FinalAPlusB;
 wire second_carry;
-full_adder first_bit_two(
-    .A(APlusB[0]), // Adding LSB of (A + B)
-    .B(1'b0), // We are adding 0, with the optional carry:
-    .Y(FinalAPlusB[0]), // This is now the real summation
-    .Cin(around), // This now takes *in* from the last sum
-    .Cout(second_carry) // We still need to carry to second
-    // bit of second addition
-);
 wire third_carry;
-full_adder second_bit_two(
-    .A(APlusB[1]), // Adding LSB of (A + B)
-    .B(1'b0), // We are adding 0, with the optional carry:
-    .Y(FinalAPlusB[1]), // This is now the real summation
-    .Cin(second_carry), // This now takes *in* from the last sum
-    .Cout(third_carry) // We still need to carry to second
-    // bit of second addition
-);
-
 wire fourth_carry;
-full_adder third_bit_two(
-    .A(APlusB[2]), // Adding LSB of (A + B)
-    .B(1'b0), // We are adding 0, with the optional carry:
-    .Y(FinalAPlusB[2]), // This is now the real summation
-    .Cin(third_carry), // This now takes *in* from the last sum
-    .Cout(fourth_carry) // We still need to carry to second
-    // bit of second addition
+
+
+full_adder first_bit_two(
+    .A(APlusB[0]),
+    .B(1'b0),
+    .Y(FinalAPlusB[0]),
+    .Cin(around), 
+    .Cout(second_carry)
+);
+    
+
+full_adder second_bit_two(
+    .A(APlusB[1]),
+    .B(1'b0), 
+    .Y(FinalAPlusB[1]),
+    .Cin(second_carry), 
+    .Cout(third_carry)
 );
 
+
+full_adder third_bit_two(
+    .A(APlusB[2]),
+    .B(1'b0),
+    .Y(FinalAPlusB[2]),
+    .Cin(third_carry),
+    .Cout(fourth_carry)
+);
+
+    
  full_adder fourth_bit_two(
         .A(APlusB[3]),
         .B(1'b0),
         .Y(FinalAPlusB[3]),
         .Cin(fourth_carry)
-        // no carry out!
+        // no final carry out
     );
 
+    
 assign Y = FinalAPlusB;
 
-
 endmodule
+
+
