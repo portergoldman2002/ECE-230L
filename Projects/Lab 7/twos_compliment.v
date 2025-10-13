375bc0818a49be5378c5947816cca06464084156
Porter Goldman <porter.goldman@outlook.com>
2025-10-12 23:20:47 -0600

    Update twos_compliment.v

--- Projects/Lab 7/twos_compliment.v
+++ Projects/Lab 7/twos_compliment.v
@@ -1,14 +1,17 @@
 
+
 module twos_compliment(
     input [7:0]A,
     output [7:0]Y
-
 );
+
+    
     wire [7:0] invertedA;
     wire [7:0] sum;
     wire [7:0] carry;
     
     assign invertedA = ~A;
+
     
     full_adder fa0(
         .A(invertedA[0]), 
@@ -16,7 +19,8 @@ module twos_compliment(
         .Y(sum[0]), 
         .Cout(carry[0])
      );
-     
+
+    
     genvar i;
     generate 
         for (i = 1; i < 8;i = i + 1)begin
@@ -28,6 +32,10 @@ module twos_compliment(
              );
         end
     endgenerate
+
     
     assign Y = sum;
+    
 endmodule
+
+
